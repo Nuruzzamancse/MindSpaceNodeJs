c92b50313d59c864c783d4dd4e43dde2279beee5
Nuruzzaman Khan Nabwab <mr.nuruzzamancse@gmail.com>
2017-02-21 20:05:51 +0600

    SecondTutorial

--- 02-node-javascript/app.js
+++ 02-node-javascript/app.js
@@ -1,8 +1,11 @@
 var http = require('http');
+var module1 = require('./module1');
+var module2 = require('./module2');
 
 function onRequest(reque,respo) {
     respo.writeHead(200,{'content-type':'text/plain'});
-    respo.write('Hello World');
+    respo.write(module2.myVariable);
+    module2.myFunction();
     respo.end();
 
 }
